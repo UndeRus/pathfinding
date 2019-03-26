4ccd068e97dae00b0bade736372155c8535065aa
imamedov <Ilyas.Mamedov@bia-tech.ru>
2019-03-26 18:27:50 +0300

    New astar2 func

--- src/directed/astar.rs
+++ src/directed/astar.rs
@@ -145,6 +145,77 @@ where
     None
 }
 
+/// Modified astar func with any result and length in result
+pub fn astar2<N, C, FN, IN, FH, FS>(
+    start: &N,
+    mut successors: FN,
+    mut heuristic: FH,
+    mut success: FS,
+) -> Option<(Vec<N>, C)>
+where
+    N: Eq + Hash + Clone,
+    C: Zero + Ord + Copy,
+    FN: FnMut(&N) -> IN,
+    IN: IntoIterator<Item = (N, C)>,
+    FH: FnMut(&N) -> C,
+    FS: FnMut(&N, usize) -> bool,
+{
+    let mut to_see = BinaryHeap::new();
+    to_see.push(SmallestCostHolder {
+        estimated_cost: heuristic(start),
+        cost: Zero::zero(),
+        index: 0,
+    });
+    let mut parents: IndexMap<N, (usize, C)> = IndexMap::new();
+    parents.insert(start.clone(), (usize::MAX, Zero::zero()));
+    while let Some(SmallestCostHolder { cost, index, .. }) = to_see.pop() {
+        let successors = {
+            let (node, &(_, c)) = parents.get_index(index).unwrap();
+            if success(node, parents.len()) {
+                let path = reverse_path(&parents, |&(p, _)| p, index);
+                return Some((path, cost));
+            }
+            // We may have inserted a node several time into the binary heap if we found
+            // a better way to access it. Ensure that we are currently dealing with the
+            // best path and discard the others.
+            if cost > c {
+                continue;
+            }
+            successors(node)
+        };
+        for (successor, move_cost) in successors {
+            let new_cost = cost + move_cost;
+            let h; // heuristic(&successor)
+            let n; // index for successor
+            match parents.entry(successor) {
+                Vacant(e) => {
+                    h = heuristic(e.key());
+                    n = e.index();
+                    e.insert((index, new_cost));
+                }
+                Occupied(mut e) => {
+                    if e.get().1 > new_cost {
+                        h = heuristic(e.key());
+                        n = e.index();
+                        e.insert((index, new_cost));
+                    } else {
+                        continue;
+                    }
+                }
+            }
+
+            to_see.push(SmallestCostHolder {
+                estimated_cost: new_cost + h,
+                cost: new_cost,
+                index: n,
+            });
+        }
+    }
+    let path = reverse_path(&parents, |&(p, _)| p, parents.len() - 1);
+    return Some((path, Zero::zero()));
+}
+
+
 /// Compute all shortest paths using the [A* search
 /// algorithm](https://en.wikipedia.org/wiki/A*_search_algorithm). Whereas `astar`
 /// (non-deterministic-ally) returns a single shortest path, `astar_bag` returns all shortest paths
